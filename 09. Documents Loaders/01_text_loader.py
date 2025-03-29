27435ab72dac54bc50556b63d69b36372c714f12
Rahul Chauhan <rahulchauhan4708@gmail.com>
2025-03-29 18:26:57 +0530

    document loaders

---
+++ 09. Documents Loaders/01_text_loader.py
@@ -0,0 +1,41 @@
+from langchain_community.document_loaders import TextLoader
+from langchain_openai import ChatOpenAI
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+# Get API key
+api_key = os.getenv("OPENAI_API_KEY")
+
+# Use ChatOpenAI instead of OpenAI
+model = ChatOpenAI(
+    api_key=api_key,
+    model='gpt-4o',
+    openai_api_base="https://api.sree.shop/v1"  # Custom endpoint
+)
+
+prompt = PromptTemplate(
+    template='Write a summary for the following poem - \n {poem}',
+    input_variables=['poem']
+)
+
+parser = StrOutputParser()
+
+loader = TextLoader('cricket.txt', encoding='utf-8')
+
+docs = loader.load()
+
+print(type(docs))
+
+print(len(docs))
+
+print(docs[0].page_content)
+
+print(docs[0].metadata)
+
+chain = prompt | model | parser
+
+print(chain.invoke({'poem':docs[0].page_content}))
